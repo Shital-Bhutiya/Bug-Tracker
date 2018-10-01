f253e2ab8267f9e259fa5bc2babbf22dfff14152
Shital Bhutiya <ssbhutiya@gmail.com>
2018-10-01 14:09:50 -0500

    Added project classs

---
+++ BugTracker/Models/Classes/Project.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTracker.Models.Classes
+{
+    public class Project
+    {
+        public Project()
+        {
+            Users = new HashSet<ApplicationUser>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<ApplicationUser> Users { get; set; }
+    }
+}
--- BugTracker/Models/IdentityModels.cs
+++ BugTracker/Models/IdentityModels.cs
@@ -1,6 +1,8 @@
-﻿using System.Data.Entity;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using BugTracker.Models.Classes;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
@@ -9,6 +11,12 @@ namespace BugTracker.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public  ICollection<Project> Project { get; set; }
+
+        public ApplicationUser()
+        {
+            Project = new HashSet<Project>();
+        }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
