206f497b04d22cfc6819c8337ba5738c2d49e2f3
Shital Bhutiya <ssbhutiya@gmail.com>
2018-10-03 10:34:46 -0500

    Added templete and admin user

---
+++ BugTracker/Migrations/Configuration.cs
@@ -0,0 +1,50 @@
+namespace BugTracker.Migrations
+{
+    using BugTracker.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BugTracker.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(BugTracker.Models.ApplicationDbContext context)
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            
+            if (!context.Roles.Any(r => r.Name == "Admin"))
+            {
+                roleManager.Create(new IdentityRole { Name = "Admin" });
+            }
+            
+            ApplicationUser adminUser = null;
+            if (!context.Users.Any(p => p.UserName == "admin@mybugtracker.com"))
+            {
+                adminUser = new ApplicationUser();
+                adminUser.UserName = "admin@mybugtracker.com";
+                adminUser.Email = "admin@mybugtracker.com";
+                adminUser.FirstName = "Admin";
+                adminUser.LastName = "User";
+                adminUser.DisplayName = "Admin User";
+                userManager.Create(adminUser, "Password-1");
+            }
+            else
+            {
+                adminUser = context.Users.Where(p => p.UserName == "admin@mybugtracker.com")
+                    .FirstOrDefault();
+            }
+            if (!userManager.IsInRole(adminUser.Id, "Admin"))
+            {
+                userManager.AddToRole(adminUser.Id, "Admin");
+            }
+        }
+    }
+}
--- BugTracker/Models/IdentityModels.cs
+++ BugTracker/Models/IdentityModels.cs
@@ -12,6 +12,9 @@ namespace BugTracker.Models
     public class ApplicationUser : IdentityUser
     {
         public  ICollection<Project> Project { get; set; }
+        public string FirstName { get;  set; }
+        public string LastName { get; set; }
+        public string DisplayName { get; set; }
 
         public ApplicationUser()
         {
