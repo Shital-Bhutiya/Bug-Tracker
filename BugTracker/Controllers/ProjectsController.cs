58893251d2106230eff44a62d6f9f338f404b38c
Shital Bhutiya <ssbhutiya@gmail.com>
2018-10-05 09:03:13 -0500

    created assign user to project functionality

--- BugTracker/Controllers/ProjectsController.cs
+++ BugTracker/Controllers/ProjectsController.cs
@@ -116,7 +116,41 @@ namespace BugTracker.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
-
+        public ActionResult AssignUsers(int id)
+        {
+            var model = new ProjectAssignViewModel();
+            model.Id = id;
+            var project = db.Projects.FirstOrDefault(p => p.Id == id);
+            var users = db.Users.ToList();
+            var userIdsAssignedToProject = project.Users
+                .Select(p => p.Id).ToList();
+            model.UserList = new MultiSelectList(users, "Id", "Name", userIdsAssignedToProject);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult AssignUsers(ProjectAssignViewModel model)
+        {
+            //STEP 1: Find the project
+            var project = db.Projects.FirstOrDefault(p => p.Id == model.Id);
+            //STEP 2: Remove all assigned users from this project
+            var assignedUsers = project.Users.ToList();
+            foreach (var user in assignedUsers)
+            {
+                project.Users.Remove(user);
+            }
+            //STEP 3: Assign users to the project
+            if (model.SelectedUsers != null)
+            {
+                foreach (var userId in model.SelectedUsers)
+                {
+                    var user = db.Users.FirstOrDefault(p => p.Id == userId);
+                    project.Users.Add(user);
+                }
+            }
+            //STEP 4: Save changes to the database
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
         protected override void Dispose(bool disposing)
         {
             if (disposing)
---
+++ BugTracker/Models/ProjectAssignViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BugTracker.Models
+{
+    public class ProjectAssignViewModel
+    {
+        public int Id { get; set; }
+        public MultiSelectList UserList { get; set; }
+        public string[] SelectedUsers { get; set; }
+    }
+}
