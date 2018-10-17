1973200dba651a4e924dd2e9cb0098a5191eb3eb
Shital Bhutiya <ssbhutiya@gmail.com>
2018-10-17 11:26:06 -0500

    Added histories comment and attechment to view

--- BugTracker/Controllers/TicketsController.cs
+++ BugTracker/Controllers/TicketsController.cs
@@ -74,7 +74,7 @@ namespace BugTracker.Controllers
                 }
                 else if (role.Contains("Submitter"))
                 {
-                    return View(db.Tickets.Include(t=>t.Comments).Include(t => t.TicketPriority).Include(t => t.TicketProject).Include(t => t.TicketStatus).Include(t => t.TicketType).Where(p => p.CreatorId == userId).ToList());
+                    return View(db.Tickets.Include(t => t.Comments).Include(t => t.TicketPriority).Include(t => t.TicketProject).Include(t => t.TicketStatus).Include(t => t.TicketType).Where(p => p.CreatorId == userId).ToList());
                 }
             }
             ViewBag.User = "";
@@ -156,22 +156,70 @@ namespace BugTracker.Controllers
         {
             if (ModelState.IsValid)
             {
+                var dateChanged = DateTimeOffset.Now;
+                var changes = new List<History>();
+
                 var dbTicket = db.Tickets.FirstOrDefault(p => p.Id == ticket.Id);
+
+
                 dbTicket.Title = ticket.Title;
                 dbTicket.Description = ticket.Description;
                 dbTicket.AssignId = ticket.AssignId;
-                dbTicket.Updated = DateTime.Now;
+                dbTicket.Updated = dateChanged;
                 dbTicket.TicketPriorityId = ticket.TicketPriorityId;
                 dbTicket.TicketProjectId = ticket.TicketProjectId;
                 dbTicket.TicketStatusId = ticket.TicketStatusId;
                 dbTicket.TicketTypeId = ticket.TicketTypeId;
+
+                var originalValues = db.Entry(dbTicket).OriginalValues;
+                var currentValues = db.Entry(dbTicket).CurrentValues;
+
+                foreach (var property in originalValues.PropertyNames)
+                {
+                    var originalValue = originalValues[property]?.ToString();
+                    var currentValue = currentValues[property]?.ToString();
+                    if (originalValue != currentValue)
+                    {
+                        var history = new History();
+                        history.ChangedDate = dateChanged;
+                        history.NewValue = GetValueFromKey(property, currentValue);
+                        history.OldValue = GetValueFromKey(property, originalValue);
+                        history.Property = property;
+                        history.TicketHistoryId = dbTicket.Id;
+                        history.HistoryUserId = User.Identity.GetUserId();
+                        changes.Add(history);
+                    }
+                }
+
+                db.Histories.AddRange(changes);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-
+            ViewBag.AssigneeId = new SelectList(db.Users, "Id", "Name", ticket.AssignId);
+            ViewBag.CreatorId = new SelectList(db.Users, "Id", "Name", ticket.CreatorId);
+            ViewBag.ProjectId = new SelectList(db.Projects, "Id", "Name", ticket.TicketProjectId);
+            ViewBag.TicketPriorityId = new SelectList(db.Prorities, "Id", "Name", ticket.TicketPriorityId);
+            ViewBag.TicketStatusId = new SelectList(db.Statues, "Id", "Name", ticket.TicketStatusId);
+            ViewBag.TicketTypeId = new SelectList(db.Types, "Id", "Name", ticket.TicketTypeId);
             return View(ticket);
         }
-
+        private string GetValueFromKey(string propertyName, string key)
+        {
+            switch (propertyName)
+            {
+                case "TicketTypeId":
+                    return db.Types.Find(Convert.ToInt32(key)).Name;
+                case "TicketStatusId":
+                    return db.Statues.Find(Convert.ToInt32(key)).Name;
+                case "TicketPriorityId":
+                    return db.Prorities.Find(Convert.ToInt32(key)).Name;
+                case "TicketProjectId":
+                    return db.Projects.Find(Convert.ToInt32(key)).Name;
+                default:
+                    break;
+            }
+            return key;
+        }
         // GET: Tickets/Delete/5
         public ActionResult Delete(int? id)
         {
@@ -230,7 +278,7 @@ namespace BugTracker.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult UploadDocument(int id ,[Bind(Include = "Id,Description,FilePath")]Attechments attechments, HttpPostedFileBase document)
+        public ActionResult UploadDocument(int id, [Bind(Include = "Id,Description,FilePath")]Attechments attechments, HttpPostedFileBase document)
         {
             if (ModelState.IsValid)
             {
